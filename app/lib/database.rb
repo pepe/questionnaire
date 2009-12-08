62161284fae63176b89567402e51e195452538ef
Josef Pospíšil <pepe@gravastar.cz>
2009-12-08 11:50:59 +0100

    Fixed environment switching

--- app/lib/database.rb
+++ app/lib/database.rb
@@ -4,7 +4,7 @@ module Questionnaire
     include Singleton
     # returns connection string
     def self.connection_string
-      if @environment == :test
+      if environment == :test
         "http://127.0.0.1:5984/test-questionnaires" 
       else
         "http://127.0.0.1:5984/questionnaires" 
@@ -18,12 +18,12 @@ module Questionnaire
 
     # returns environment
     def self.environment
-      @environment
+      @environment ||= :net
     end
 
     # returns connection
     def self.connection
-      @connection ||= CouchRest.database!(connection_string)
+      @connection = CouchRest.database!(connection_string)
     end
   end
 end
--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -1,12 +1,16 @@
 require 'app/lib/database'
 describe "Database" do
   context "retrieving connection string based on environment" do
+    Questionnaire::Database.environment = :net
     Questionnaire::Database.connection_string.should == "http://127.0.0.1:5984/questionnaires"
     Questionnaire::Database.environment = :test
     Questionnaire::Database.connection_string.should == "http://127.0.0.1:5984/test-questionnaires"
   end
 
   context "retrieving database" do
-    Questionnaire::Database.connection == CouchRest.database!("http://127.0.0.1:5984/questionnaires")
+    Questionnaire::Database.environment = :net
+    Questionnaire::Database.connection.name.should == CouchRest.database("http://127.0.0.1:5984/questionnaires").name
+    Questionnaire::Database.environment = :test
+    Questionnaire::Database.connection.name.should == CouchRest.database("http://127.0.0.1:5984/test-questionnaires").name
   end
 end
